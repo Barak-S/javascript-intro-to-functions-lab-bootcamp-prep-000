dea21c44f05dd5801bbf8d4e49da80b60e2cb10e
Barak Saidoff <barak.saidoff@gmail.com>
2019-08-12 14:10:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,5 @@ function logWhisper(string) {
   return console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
-  if (string === 'hello'){
-    return console.log("I can't hear you!")
-  } else if (string === 'HELLO'){
-    return console.log("YES INDEED!")
-  } else if (string === 'I love you, Grandma.') {
-    return console.log('I love you, too.')
-  }
+  
 }
